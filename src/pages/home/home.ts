f3487584a964989dfb7334a7e77cb8009fd01a3d
gityalvarez <yamandu.alvarez@gmail.com>
2017-11-01 19:46:40 -0300

    Commit inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestapiServiceProvider } from '../../providers/restapi-service/restapi-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+productos: any;
+
+
+  constructor(public navCtrl: NavController, public restapiService: RestapiServiceProvider) {
+  this.getProductos();
 
   }
+
+
+getProductos() {
+  this.restapiService.getProductos()
+  .then(data => {
+    this.productos = data;
+    console.log(this.productos);
+  });
+}
+
 
 }
---
+++ src/providers/restapi-service/restapi-service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the RestapiServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestapiServiceProvider {
+
+data:any;
+apiUrl ='http://localhost:8000/api/v1/producto?access_token=0PMHlJ6SP2ABmQ2VAIaYC0wvgxU6bgz2doLxlOIt';
+
+constructor(public http: Http) {
+    console.log('Hello RestapiServiceProvider Provider');
+  }
+
+getProductos() {
+  if (this.data) {
+    return Promise.resolve(this.data);
+  }
+
+  return new Promise(resolve => {
+    this.http.get(this.apiUrl)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.data = data;
+        resolve(this.data);
+      });
+  });
+}
+
+  
+
+}
